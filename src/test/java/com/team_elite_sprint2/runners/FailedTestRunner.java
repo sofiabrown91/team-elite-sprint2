499648b0722971c6e73bb0465ab9cde9236d4827
sofiabrown91 <bilgehanbulbul91@gmail.com>
2024-03-18 12:20:59 -0400

    Initial commit

---
+++ src/test/java/com/team_elite_sprint2/runners/FailedTestRunner.java
@@ -0,0 +1,15 @@
+package com.team_elite_sprint2.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "@target/rerun.txt",
+        glue = "com/team_elite_sprint2/step_definitions"
+)
+public class FailedTestRunner {
+}
+
+
