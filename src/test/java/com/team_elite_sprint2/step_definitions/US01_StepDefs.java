ddd546af5a4b04e48197451163244a213c1e11f9
celikerCydeo <celik+celik.eren57@hotmail.com>
2024-03-20 17:46:50 -0400

    US01_Main Module Options, Test Scripts, and Test Cases

--- src/test/java/com/team_elite_sprint2/step_definitions/US01_StepDefs.java
+++ src/test/java/com/team_elite_sprint2/step_definitions/US01_StepDefs.java
@@ -1,59 +1,33 @@
 package com.team_elite_sprint2.step_definitions;
 
+import com.team_elite_sprint2.pages.DashboardMenuOptionsPage;
 import com.team_elite_sprint2.utilities.BrowserUtils;
 import io.cucumber.java.en.Then;
 import org.junit.Assert;
-import org.openqa.selenium.WebElement;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class US01_StepDefs {
 
-    // This snippet is take care of users sales manager and store manager verifications of menu option's visibility.
+
+    DashboardMenuOptionsPage dashboardPage = new DashboardMenuOptionsPage();
 
     @Then("the user should see the following options")
-    public void the_user_should_see_the_following_options(List<String> expectedTopModules) {
-
-        LoginStepDefs dashboardPage = new LoginStepDefs();
-
-
+    public void verifyMenuOptionsVisibility(List<String> expectedTopModules) {
         BrowserUtils.waitForTitleContains("Dashboard");
-
-        List<String> actualTopModules = new ArrayList<>();
-
-        for (WebElement eachTopModule : dashboardPage.menuOptions) {
-            actualTopModules.add(eachTopModule.getText());
-
-        }
-
-        Assert.assertEquals(expectedTopModules, actualTopModules);
+        verifyMenuOptions(expectedTopModules);
 
     }
-
-// This snippet is take care of verification of driver users menu option's visibility.
 
     @Then("user should see the following options")
-    public void user_should_see_the_following_options(List<String> expectedTopModules) {
-
-
-        LoginStepDefs dashboardPage = new LoginStepDefs();
-
-
+    public void verifyMenuOptionsVisibilityForDriverUser(List<String> expectedTopModules) {
         BrowserUtils.waitFor(10);
-
-        List<String> actualTopModules = new ArrayList<>();
-
-        for (WebElement eachTopModule : dashboardPage.menuOptions) {
-            actualTopModules.add(eachTopModule.getText());
-
-        }
-
-        Assert.assertEquals(actualTopModules, expectedTopModules);
-
-
+        verifyMenuOptions(expectedTopModules);
     }
 
-
+    private void verifyMenuOptions(List<String> expectedTopModules) {
+        List<String> actualTopModules = BrowserUtils.getElementsText(dashboardPage.menuOptions);
+        Assert.assertEquals(expectedTopModules, actualTopModules);
+    }
 
 }
