e32ab3376d1bd35f38035e9403f5a9af6e8ba82a
Karshyga1985 <kbayatova@gmail.com>
2024-03-21 16:09:03 -0400

    update scenario name for US08

--- src/test/java/com/team_elite_sprint2/runners/CukesRunner.java
+++ src/test/java/com/team_elite_sprint2/runners/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/team_elite_sprint2/step_definitions",
         dryRun = false,
-        tags = "",
+        tags = "@US08",
         publish = true //generating a report with public link
 )
 public class CukesRunner {}
