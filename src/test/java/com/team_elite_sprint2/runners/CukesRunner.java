45c0f66da3a60c327ec9bdddd73fda8b4ca3e8d1
sofiabrown91 <bilgehanbulbul91@gmail.com>
2024-03-21 17:07:25 -0400

    Added US5 and US7

--- src/test/java/com/team_elite_sprint2/runners/CukesRunner.java
+++ src/test/java/com/team_elite_sprint2/runners/CukesRunner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/team_elite_sprint2/step_definitions",
         dryRun = false,
-        tags = "@vehicle",
+        tags = "",
         publish = true //generating a report with public link
 )
 public class CukesRunner {}
